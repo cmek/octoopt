d629aa458298936d29f7c2036aacc33a5aee5064
Szymon Roczniak <simon@dischaos.com>
2026-01-04 21:17:37 +0000

    feat: integrate inverter settings with the assistant

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import asyncio
+# import asyncio
 import os
 import logging
 import argparse
 from dotenv import load_dotenv
 from agileocto import OctopusAgile
-from telegram.ext import Application
+
+# from telegram.ext import Application
 from givenergy import GivEnergyApi
+from db.models import Context
 
 OCTOPUS_PRODUCT_CODE = "AGILE-24-10-01"
 OCTOPUS_TARIFF_CODE = "E-1R-AGILE-24-10-01-G"
@@ -37,8 +39,8 @@ def main():
     )
     args = parser.parse_args()
 
-    app = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
-    group_id = os.getenv("TELEGRAM_GROUP_ID")
+    #    app = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+    #    group_id = os.getenv("TELEGRAM_GROUP_ID")
     giveapi = GivEnergyApi(
         os.getenv("GIVENERGY_API_KEY"), os.getenv("INVERTER_SERIAL_NUMBER")
     )
@@ -51,16 +53,8 @@ def main():
     )
 
     battery_charge_window = api.get_battery_charge_window()
-    peak_time_window = api.get_peak_time(slots=1)
+    peak_time_window = api.get_peak_time(slots=0)
     savings = peak_time_window["total_cost"] - battery_charge_window["total_cost"]
-
-    message = f"""battery charge window: {battery_charge_window["start_time"]} - {battery_charge_window["end_time"]} {battery_charge_window["start_time_hour"]} - {battery_charge_window["end_time_hour"]}
-battery charge cost: {battery_charge_window["total_cost"]:.3f}p (@ {battery_charge_window["avg_cost_per_khw"]:.3f} p/kWh)
-
-peak time window: {peak_time_window["start_time"]} - {peak_time_window["end_time"]} {peak_time_window["start_time_hour"]} - {peak_time_window["end_time_hour"]}
-peak time cost: {peak_time_window["total_cost"]:.3f}p (@ {peak_time_window["avg_cost_per_khw"]:.3f} p/kWh)
-savings: {savings:.3f}p"""
-    print(message)
 
     if args.dry_run:
         print("DRY RUN: The following inverter settings would be applied:")
@@ -92,7 +86,22 @@ savings: {savings:.3f}p"""
             "Pause Battery End Time", peak_time_window["start_time_hour"]
         )
 
-        asyncio.run(send_message(app.bot, group_id, message))
+    message = f"""These are the inverter settings for tomorrow based on prices from Octopus Agile tariff. Add these to the brief:
+
+Battery will be charged between {battery_charge_window["start_time_hour"]} - {battery_charge_window["end_time_hour"]}. The total cost to charge battery is 
+£{battery_charge_window["total_cost"] / 100:.3f} (average kWh cost over this time period is: £{battery_charge_window["avg_cost_per_khw"] / 100:.3f})
+
+The peak time tomorrow is between {peak_time_window["start_time_hour"]} - {peak_time_window["end_time_hour"]}. This is when the house should be running on battery only so avoid running additional appliances like dishwashers or washing/drying machine at this time.
+Peak time energy cost for 9.5kWh will be £{peak_time_window["total_cost"] / 100:.3f} (@ £{peak_time_window["avg_cost_per_khw"] / 100:.3f} per kWh)
+This represents about £{savings / 100:.3f} in savings"""
+    logger.info(message)
+    Context.objects.filter(tags__contains=["power_settings", "brief"]).delete()
+    Context.objects.create(
+        context=message, created_by="power_settings", tags=["power_settings", "brief"]
+    )
+
+
+#        asyncio.run(send_message(app.bot, group_id, message))
 
 
 if __name__ == "__main__":
